c9e15e2f187adac78eeb61277a454725e7d2de69
srlnp <srln.patro@gmail.com>
2018-09-09 11:46:34 +0530

    extra code added

--- server1.js
+++ server1.js
@@ -5,3 +5,33 @@ app.use(express.static(__dirname+'/index1.html'));
 console.log(__dirname+'/index1.html')
 app.listen(port);
 console.log("server started"+port);
+
+var bodyparser=require("body-parser")
+var mysql=require("mysql");
+var conn=require("./db_connection");
+var express=require("express");
+var app=express();
+app.get("/",function(req,res){
+var connection=conn.getconnection();
+connection.connect(); 
+connection.query("select * from empn",function(err,result,fields){
+if(err)
+{
+console.log(err);
+console.log("error connection on database");
+ res.send("error on connection on database");
+}else
+{if(result=='')
+    {
+        res.send("sorry data not exist sorry");
+    } 
+    else{
+        res.send(result);
+        res.end();}}});});
+app.listen(3333,function(){
+console.log("sir your server listning the prot no 3333");
+});
+
+
+
+
